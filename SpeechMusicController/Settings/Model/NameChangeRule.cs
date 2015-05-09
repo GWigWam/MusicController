eabb0860bd0d64e136d1419393147fb0b4cd22ba
GWigWam <welwisam@gmail.com>
2015-05-09 16:36:08 +0200

    Settings Model & Newtonsoft's JSon lib

---
+++ SpeechMusicController/Settings/Model/NameChangeRule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechMusicController.Settings.Model {
+    internal class NameChangeRule : SongRule {
+        public readonly string NewName;
+
+        public NameChangeRule(string title, string artist, string album, string newName)
+            : base(title, artist, album) {
+            if (string.IsNullOrEmpty(newName)) {
+                throw new ArgumentException("NewName");
+            }
+
+            NewName = newName;
+        }
+    }
+}
