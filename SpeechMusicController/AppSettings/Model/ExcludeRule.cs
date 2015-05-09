2caba3ca4692cd0f82891f727e9068f39e4e8e29
GWigWam <welwisam@gmail.com>
2015-05-09 17:46:38 +0200

    Updated Model w/ Type enum

---
+++ SpeechMusicController/AppSettings/Model/ExcludeRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechMusicController.AppSettings.Model {
+    internal class ExcludeRule : SongRule {
+
+        public ExcludeRule(string title, string artist, string album)
+            : base(title, artist, album, SongRuleType.Exclude) {
+        }
+    }
+}
---
+++ SpeechMusicController/AppSettings/Model/SongRule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechMusicController.AppSettings.Model {
+    public abstract class SongRule {
+        public readonly string Title;
+        public readonly string Artist;
+        public readonly string Album;
+
+        public readonly SongRuleType Type;
+
+        public SongRule(string title, string artist, string album, SongRuleType type) {
+            Title = title ?? string.Empty;
+            Artist = artist ?? string.Empty;
+            Album = album ?? string.Empty;
+
+            Type = type;
+        }
+    }
+
+    public enum SongRuleType {
+        Exclude, NameChange
+    }
+}
