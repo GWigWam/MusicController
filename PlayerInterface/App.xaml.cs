8b944867f1164613b0043a3b3a4571b62bdd084a
GWigWam <welwisam@gmail.com>
2016-01-19 16:21:09 +0100

    Moved window init/hide/show to new class

--- PlayerInterface/App.xaml.cs
+++ PlayerInterface/App.xaml.cs
@@ -10,54 +10,14 @@ using System.Windows;
 namespace PlayerInterface {
 
     public partial class App : Application {
-        private TaskbarIcon TrayIcon;
-        private FullPlayer Full;
-        private SmallPlayer Small;
+
+        internal WindowManager WindowMgr {
+            get; private set;
+        }
 
         private void Application_Startup(object sender, StartupEventArgs e) {
-            Init();
-            TrayIcon.TrayMouseDoubleClick += (s, a) => ShowFullWindow();
-            TrayIcon.TrayMouseMove += Tbi_TrayMouseMove;
-        }
-
-        private void Init() {
-            TrayIcon = (TaskbarIcon)FindResource("Tbi_Icon");
-            CreateSmallPlayer(true);
-            CreateFullPlayer(false);
-        }
-
-        private void CreateSmallPlayer(bool show) {
-            Small = new SmallPlayer();
-            Small.Btn_ShowFull.MouseLeftButtonUp += (s, a) => ShowFullWindow();
-            if(show)
-                Small.Show();
-        }
-
-        private void CreateFullPlayer(bool show) {
-            Full = new FullPlayer();
-            Full.MinimizedToTray += (s, a) => ShowSmallWindow();
-            if(show)
-                Full.Show();
-        }
-
-        private void Tbi_TrayMouseMove(object sender, RoutedEventArgs e) {
-            if(!(Full?.IsVisible ?? false)) {
-                ShowSmallWindow();
-            }
-        }
-
-        private void ShowSmallWindow() {
-            Small.Show();
-            Small.Activate();
-        }
-
-        private void ShowFullWindow() {
-            if(!Full.IsVisible) {
-                Full.WindowState = WindowState.Normal;
-            }
-            Full.Show();
-            Full.Activate();
-            Small.Hide();
+            WindowMgr = new WindowManager(this);
+            WindowMgr.Init();
         }
     }
 }
---
+++ PlayerInterface/WindowManager.cs
@@ -0,0 +1,67 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PlayerInterface {
+
+    internal class WindowManager {
+
+        private Application App {
+            get;
+        }
+
+        private TaskbarIcon TrayIcon;
+        private FullPlayer Full;
+        private SmallPlayer Small;
+
+        public WindowManager(Application app) {
+            App = app;
+        }
+
+        public void Init() {
+            TrayIcon = (TaskbarIcon)App.FindResource("Tbi_Icon");
+            CreateSmallPlayer(true);
+            CreateFullPlayer(false);
+            TrayIcon.TrayMouseDoubleClick += (s, a) => ShowFullWindow();
+            TrayIcon.TrayMouseMove += Tbi_TrayMouseMove;
+        }
+
+        private void CreateSmallPlayer(bool show) {
+            Small = new SmallPlayer();
+            Small.Btn_ShowFull.MouseLeftButtonUp += (s, a) => ShowFullWindow();
+            if(show)
+                Small.Show();
+        }
+
+        private void CreateFullPlayer(bool show) {
+            Full = new FullPlayer();
+            Full.MinimizedToTray += (s, a) => ShowSmallWindow();
+            if(show)
+                Full.Show();
+        }
+
+        private void Tbi_TrayMouseMove(object sender, RoutedEventArgs e) {
+            if(!(Full?.IsVisible ?? false)) {
+                ShowSmallWindow();
+            }
+        }
+
+        private void ShowSmallWindow() {
+            Small.Show();
+            Small.Activate();
+        }
+
+        private void ShowFullWindow() {
+            if(!Full.IsVisible) {
+                Full.WindowState = WindowState.Normal;
+            }
+            Full.Show();
+            Full.Activate();
+            Small.Hide();
+        }
+    }
+}
