fd2ccf5afda5bc2169eb716ba842c7cc1e40200c
GWigWam <welwisam@gmail.com>
2018-01-20 21:40:31 +0100

    Rewrite GenericRelayCommand<T>

--- PlayerInterface/Commands/GenericRelayCommand.cs
+++ PlayerInterface/Commands/GenericRelayCommand.cs
@@ -6,48 +6,35 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace PlayerInterface.Commands {
+    public class GenericRelayCommand<T> : ICommand {
 
-    public class GenericRelayCommand<T> : ICommand where T : class {
         private Action<T> execute;
-
         private Predicate<T> canExecute;
 
         private event EventHandler CanExecuteChangedInternal;
-
-        public GenericRelayCommand(Action<T> execute, Action<Exception> exeptionHandler = null)
-            : this(execute, DefaultCanExecute, exeptionHandler) {
-        }
-
-        public GenericRelayCommand(Action<T> execute, Predicate<T> canExecute, Action<Exception> exeptionHandler = null) {
-            if(execute == null) {
-                throw new ArgumentNullException("execute");
-            }
-
-            if(canExecute == null) {
-                throw new ArgumentNullException("canExecute");
-            }
-
-            this.execute = execute;
-            this.canExecute = canExecute;
-            ExceptionHandler = exeptionHandler;
-        }
-
         public event EventHandler CanExecuteChanged {
             add {
                 CommandManager.RequerySuggested += value;
-                this.CanExecuteChangedInternal += value;
+                CanExecuteChangedInternal += value;
             }
 
             remove {
                 CommandManager.RequerySuggested -= value;
-                this.CanExecuteChangedInternal -= value;
+                CanExecuteChangedInternal -= value;
             }
         }
 
         public Action<Exception> ExceptionHandler;
 
-        public bool CanExecute(object parameter) {
-            return (parameter as T) != null && this.canExecute != null && this.canExecute((T)parameter);
+        public bool CanExecute(object parameter) => parameter is T tParam && canExecute(tParam);
+
+        public GenericRelayCommand(Action<T> execute, Action<Exception> exeptionHandler = null)
+            : this(execute, DefaultCanExecute, exeptionHandler) { }
+
+        public GenericRelayCommand(Action<T> execute, Predicate<T> canExecute, Action<Exception> exeptionHandler = null) {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
+            ExceptionHandler = exeptionHandler;
         }
 
         public void Execute(object parameter) {
@@ -58,25 +45,10 @@ namespace PlayerInterface.Commands {
             }
         }
 
-        public void Execute(T parameter) {
-            Execute(parameter as object);
-        }
+        public void Execute(T parameter) => Execute(parameter as object);
 
-        public void OnCanExecuteChanged() {
-            EventHandler handler = this.CanExecuteChangedInternal;
-            if(handler != null) {
-                //DispatcherHelper.BeginInvokeOnUIThread(() => handler.Invoke(this, EventArgs.Empty));
-                handler.Invoke(this, EventArgs.Empty);
-            }
-        }
+        public void OnCanExecuteChanged() => CanExecuteChangedInternal?.Invoke(this, EventArgs.Empty);
 
-        public void Destroy() {
-            this.canExecute = _ => false;
-            this.execute = _ => { return; };
-        }
-
-        private static bool DefaultCanExecute(T parameter) {
-            return true;
-        }
+        private static bool DefaultCanExecute(T parameter) => true;
     }
 }
