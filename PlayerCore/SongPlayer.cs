619db117840975e1ed8d69fd0f88167c9ceb23b0
GWigWam <welwisam@gmail.com>
2019-06-02 21:03:34 +0200

    Alter NAudio IWavePlayer buffer properties

--- PlayerCore/SongPlayer.cs
+++ PlayerCore/SongPlayer.cs
@@ -197,8 +197,8 @@ namespace PlayerCore {
                     // Amount of music in mem = (Latency * NrOfBuffers) because latency == size per buffer
                     // See: https://github.com/naudio/NAudio/wiki/Understanding-Output-Devices
                     Player = new WaveOutEvent() {
-                        DesiredLatency = 400, // In ms, Default = 300
-                        NumberOfBuffers = 10, // Default = 2
+                        DesiredLatency = 150, // In ms, Default = 300
+                        NumberOfBuffers = 5, // Default = 2
                         DeviceNumber = -1 // 1- for default device, 0 for first device
                     };
 
