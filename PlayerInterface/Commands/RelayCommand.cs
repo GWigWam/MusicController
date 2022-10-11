3a6e4bcf50bc14d437450a1c1a84b5aaf4530154
GWigWam <welwisam@gmail.com>
2022-10-11 19:01:15 +0200

    Rewrite (Sync) RelayCommand

--- PlayerInterface/Commands/RelayCommand.cs
+++ PlayerInterface/Commands/RelayCommand.cs
@@ -3,38 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Input;
 
-namespace PlayerInterface.Commands {
+#nullable enable
+namespace PlayerInterface.Commands
+{
+    public class RelayCommand : RelayCommand<object>
+    {
+        public RelayCommand(Action execute, Action<Exception>? exeptionHandler = null)
+            : this(execute, DefaultCanExecute, exeptionHandler) { }
 
-    public class RelayCommand : BaseCommand {
-
-        private readonly Action execute;
-        private readonly Func<bool> canExecute;
-        private readonly Action<Exception> ExceptionHandler;
-
-        public RelayCommand(Action execute, Action<Exception> exeptionHandler = null)
-            : this(execute, DefaultCanExecute, exeptionHandler) {
-        }
-
-        public RelayCommand(Action execute, Func<bool> canExecute, Action<Exception> exeptionHandler = null) {
-            this.execute = execute ?? throw new ArgumentNullException("execute");
-            this.canExecute = canExecute ?? throw new ArgumentNullException("canExecute");
-            ExceptionHandler = exeptionHandler ?? DefaultExceptionHandler;
-        }
-
-        public override bool CanExecute(object parameter) => canExecute();
-
-        public override void Execute(object parameter) {
-            try {
-                execute();
-            } catch(Exception e) {
-                ExceptionHandler(e);
-            }
-        }
+        public RelayCommand(Action execute, Func<bool> canExecute, Action<Exception>? exeptionHandler = null)
+            : base(_ => execute(), _ => canExecute(), exeptionHandler) { }
 
         private static bool DefaultCanExecute() => true;
-
-        private static void DefaultExceptionHandler(Exception e) => throw e;
     }
 }
--- PlayerInterface/Commands/RelayCommand{T}.cs
+++ PlayerInterface/Commands/RelayCommand{T}.cs
@@ -3,41 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Input;
 
-namespace PlayerInterface.Commands {
-    public class RelayCommand<T> : BaseCommand {
+#nullable enable
+namespace PlayerInterface.Commands
+{
+    public class RelayCommand<T> : BaseCommand
+    {
+        protected Action<T?> _Execute;
+        protected Predicate<T?> _CanExecute;
 
-        private Action<T> execute;
-        private Predicate<T> canExecute;
-        
-        public Action<Exception> ExceptionHandler;
+        public Action<Exception>? ExceptionHandler;
 
-        public RelayCommand(Action<T> execute, Action<Exception> exeptionHandler = null)
+        public RelayCommand(Action<T?> execute, Action<Exception>? exeptionHandler = null)
             : this(execute, DefaultCanExecute, exeptionHandler) { }
 
-        public RelayCommand(Action<T> execute, Predicate<T> canExecute, Action<Exception> exeptionHandler = null) {
-            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
-            this.canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
+        public RelayCommand(Action<T?> execute, Predicate<T?> canExecute, Action<Exception>? exeptionHandler = null)
+        {
+            _Execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _CanExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
             ExceptionHandler = exeptionHandler;
         }
 
-        public override bool CanExecute(object parameter) => (parameter is T || parameter == null) && canExecute((T)parameter);
+        public override bool CanExecute(object parameter) => (parameter is T || parameter == null) && _CanExecute((T?)parameter);
 
-        public override void Execute(object parameter) {
-            try {
-                if (parameter is T parm) {
-                    execute(parm);
-                } else {
+        public override void Execute(object? parameter)
+        {
+            try
+            {
+                if (parameter is T parm)
+                {
+                    _Execute(parm);
+                }
+                else if (parameter is null)
+                {
+                    _Execute(default);
+                }
+                else
+                {
                     throw new InvalidOperationException($"Expected parameter of type '{typeof(T).FullName}'.");
                 }
-            } catch(Exception e) {
-                ExceptionHandler?.Invoke(e);
+            }
+            catch (Exception e)
+            {
+                if (ExceptionHandler != null)
+                {
+                    ExceptionHandler(e);
+                }
+                else
+                {
+                    System.Diagnostics.Trace.WriteLine($"Unhandled exception in {nameof(RelayCommand)}: {e}");
+                }
             }
         }
 
         public void Execute(T parameter) => Execute(parameter as object);
-        
-        private static bool DefaultCanExecute(T parameter) => true;
+
+        private static bool DefaultCanExecute(T? parameter) => true;
     }
 }
